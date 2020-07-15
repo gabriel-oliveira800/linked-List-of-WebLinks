9836eb8350c10b39385e63cc851cd04c74c01cda
gabrielscorehero <gabrielscorehero55@gmail.com>
2020-07-15 13:37:07 -0300

    created struct to item and struct to web links

--- webLinks.h
+++ webLinks.h
@@ -4,5 +4,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct web Web;
+typedef struct webLinks WebLinks;
 
+WebLinks* init();
+
+void add_start(WebLinks** list_link, char* link, char* keyword);
+void add_middle(WebLinks** list_link, char* link, char* keyword);
+void add_end(WebLinks** list_link, char* link, char* keyword);
+
+void toString(WebLinks* list_link);
 #endif // WEBLINKS_H_INCLUDED
